09dbb45eb746f719709092bcd9989de371ac1378
mvtdev1050 <myvirtualteams1@gmail.com>
2024-01-24 10:11:13 +0530

    removed_key

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 export default function Card() {
+  const [allData, setAllData] = useState([])
+  useEffect(()=>{
+    console.log("Here")
+    let url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US'
+    let options = {
+      "Authorization":'Bearer',
+      "accept":'application/json'
+    }
+    let data  = fetch(url,{
+      headers: options
+    }).then(async (res)=>{
+      let data = await res.json()
+      console.log("data",data)
+      setAllData(data?.results)
+      
+    }).catch(err=> console.log("Error while fteching all data",err))
+  },[])
+
   return (
-    <div style={{border:"1px solid white"}}>
+    <>
+    {allData.lenght > 0 && 
+      allData.map((ele)=>{
+      <div style={{border:"1px solid white"}}>
         <img src="" alt="not found" width={100} height={100}></img>
-    </div>
+      </div>
+      })
+    }
+    </>
+   
   )
 }
